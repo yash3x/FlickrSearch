85c0571cce4ee2555d864058ac616c14507ba401
Yash <yashworks@gmail.com>
2018-10-10 21:32:40 +0530

    End of lesson Preparing Data Structures

--- FlickrSearch/FlickrPhotosViewController.swift
+++ FlickrSearch/FlickrPhotosViewController.swift
@@ -12,4 +12,14 @@ class FlickrPhotosViewController: UICollectionViewController {
     // MARK: - Properties
     fileprivate let reuseIdentifier = "FlickrCell"
     fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    fileprivate var searches = [FlickrSearchResults]()
+    fileprivate let flickr = Flickr()
+}
+
+// MARK: - Private
+private extension FlickrPhotosViewController {
+    func photoForIndexPath(indexPath: IndexPath) -> FlickrPhoto {
+        return searches[(indexPath as NSIndexPath).section].searchResults[(indexPath as IndexPath).row]
+    }
 }
